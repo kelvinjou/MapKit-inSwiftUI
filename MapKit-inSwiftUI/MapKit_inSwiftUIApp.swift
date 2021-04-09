3e2100a2fc057ca73b8548e037e771ffc757234f
kelvinjou <63611619+kelvinjou@users.noreply.github.com>
2021-04-08 19:50:46 -0700

    List. from A to B (both user selected points)

---
+++ MapKit-inSwiftUI/MapKit_inSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  MapKit_inSwiftUIApp.swift
+//  MapKit-inSwiftUI
+//
+//  Created by mimi on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_inSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            TrackCenterCoordinate()
+        }
+    }
+}
