be958b8f4a56037421f154007891f44ae2464579
kelvinjou <63611619+kelvinjou@users.noreply.github.com>
2021-04-16 21:05:17 -0700

    Added Reverse Geocoding Functionality

--- MapKit-inSwiftUI/TrackCenterCoordinate.swift
+++ MapKit-inSwiftUI/TrackCenterCoordinate.swift
@@ -13,23 +13,24 @@ struct TrackCenterCoordinate: View {
     
     @ObservedObject var locationManager = LocationManager()
     @State public var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.71, longitude: -82), span: MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9))
-    @State private var locations = [MKPointAnnotation]()
-//    let userLocation: MKPointAnnotation = {
-//        let mkPointAnnotation = MKPointAnnotation()
-//        mkPointAnnotation.coordinate = LocationManager().location!.coordinate
-//        print(mkPointAnnotation)
-//        return mkPointAnnotation
-//    }()
+    @State private var location = MKPointAnnotation()
+    var userLocation: MKPointAnnotation {
+        let mkPointAnnotation = MKPointAnnotation()
+        mkPointAnnotation.coordinate = LocationManager().location!.coordinate
+        print(mkPointAnnotation)
+        return mkPointAnnotation
+    }
     @State private var pin: [Pin] = []
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var turnOffOpacity: Bool = false
     @State private var advisoryNotices = [String]()
-
+    
 
     @State private var showAdvisory: Bool = false
     var mapTypes: [String] = ["standard", "hybrid", "satellite"]
-
+    @State private var addLine: Bool = false
     @State private var currentSelectedMapType = 0
+//    @ObservableObject var coordinator = Coordinator(mapView)
     func setCurentLocation() {
         locationManager.$location.sink { location in
             region = MKCoordinateRegion(center: location?.coordinate ?? CLLocationCoordinate2D(), span: MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9))
@@ -37,8 +38,9 @@ struct TrackCenterCoordinate: View {
     }
     var body: some View {
         let mapView: MapView = {
-            MapView(centerCoordinate: $centerCoordinate, currentSelectedMapType: $currentSelectedMapType, adivsoryNotices: $advisoryNotices, annotations: $locations)
+            MapView(centerCoordinate: $centerCoordinate, currentSelectedMapType: $currentSelectedMapType, adivsoryNotices: $advisoryNotices, annotation: $location, addLine: addLine)
         }()
+        
         return ZStack {
             mapView
                 .edgesIgnoringSafeArea(.all)
@@ -73,21 +75,24 @@ struct TrackCenterCoordinate: View {
                         Text("Advisory Notices")
                     }
                     Spacer()
-                    Button(action: {
-                        self.locations.removeLast()
-//                        removeLine.toggle()
-                    }) {
-                        Image(systemName: "trash")
-                    }.disabled(locations.count == 0 ? true : false)
-                    .buttonStyle(ButtonModifiers(color: locations.count == 0 ? Color.red.opacity(0.25) : Color.red.opacity(0.95)))
+//                    Button(action: {
+//
+////                        addLine = false
+//                        print(addLine)
+//                    }) {
+//                        Image(systemName: "trash")
+//                    }.disabled(false)
+//                    .buttonStyle(ButtonModifiers(color: Color.red.opacity(0.95)))
+//                    Text("\()")
                     Button(action: {
 //                        pin.append(Pin(location: CLLocation(coordinate: centerCoordinate, altitude: 0, horizontalAccuracy: 0, verticalAccuracy: 0, timestamp: Date())))
 //                        print(pin)
                         let newLocation = MKPointAnnotation()
                         newLocation.coordinate = centerCoordinate
-                        self.locations.append(newLocation)
+                        self.location = newLocation
+                        addLine = true
+                        print(self.addLine)
 //                        print(newLocation.coordinate)
-//                        let newLocation2 = MKPlacemark(coordinate: newLocation.coordinate)
                         print(newLocation.coordinate)
                     }) {
                         Image(systemName: "plus")
@@ -109,18 +114,25 @@ struct TrackCenterCoordinate: View {
 }
 
 struct MapView: UIViewRepresentable {
+    static var defaultRegion = CLLocationCoordinate2D(latitude: 40.71, longitude: -82)
     @ObservedObject var locationManager = LocationManager()
     @Binding var centerCoordinate: CLLocationCoordinate2D
     @Binding var currentSelectedMapType: Int
     @Binding var adivsoryNotices: [String]
-    @Binding var annotations: [MKPointAnnotation]
+
+    @Binding var annotation: MKPointAnnotation
+    
     @State var ETA = TimeInterval()
     
+     var addLine: Bool
+    @State var route: MKRoute?
     let mapView = MKMapView()
     func makeUIView(context: Context) -> MKMapView {
         
         mapView.delegate = context.coordinator
         mapView.showsUserLocation = true
+//        print(mapView.userLocation.coordinate)
+        mapView.userTrackingMode = .follow
         locationManager
         
 //        mapView.mapType = .satellite
@@ -128,29 +140,28 @@ struct MapView: UIViewRepresentable {
         return mapView
     }
     
-    func render(_ location: CLLocation) {
-        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
-        
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        
-        mapView.setRegion(region, animated: true)
-    }
+//    func render(_ location: CLLocation) {
+//        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+//
+//        let region = MKCoordinateRegion(center: coordinate, span: span)
+//
+//        mapView.setRegion(region, animated: true)
+//    }
+    
     
     func updateUIView(_ view: UIViewType, context: Context) {
             //annotations over here are the new annotations(passed from the SwiftUI view
         
-        if annotations.count != view.annotations.count {
+//        if annotation != view.annotations.count {
                                 //view.annotations are the old annotations
-            view.removeAnnotations(view.annotations)
-            view.addAnnotations(annotations)
-            if annotations.count >= 2 && locationManager.location != nil {
+            
+            if locationManager.location != nil {
                 let request = MKDirections.Request()
 
-//                request.source = MKMapItem(placemark: MKPlacemark(coordinate: annotations[0].coordinate))
-                request.source = MKMapItem(placemark: MKPlacemark(coordinate: locationManager.location!.coordinate))
-                print(locationManager.location?.coordinate)
-                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: annotations[1].coordinate))
+                request.source = MKMapItem(placemark: MKPlacemark(coordinate: view.userLocation.coordinate))
+//                request.source = MKMapItem(placemark: MKPlacemark(coordinate: locationManager.location!.coordinate))
+                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: annotation.coordinate))
                 request.requestsAlternateRoutes = true
                 request.transportType = .automobile
                 
@@ -159,11 +170,22 @@ struct MapView: UIViewRepresentable {
                 directions.calculate { (response, err) in
                     //          checking to make sure something is there and would assign it to "route" if response is not nil.
                     guard let route = response?.routes.first else { return }
-                    view.addAnnotations([annotations[0], annotations[1]])
-
+                    
+                    if let oldRoute = self.route {
+                        print("here")
+                        view.removeAnnotation(annotation)
+                        view.removeOverlay(oldRoute.polyline)
+                        
+//                        view.dequeueReusableAnnotationView(withIdentifier: "point")
+                    }
+                    self.route = route
+                    
                     //          a route that would be drawn between these 2 places
-                    view.addOverlay(route.polyline, level: .aboveRoads)
+                    view.addOverlay(self.route!.polyline, level: .aboveRoads)
+                    view.addAnnotation(annotation)
+//                    self.addLine = true
                     view.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40), animated: true)
+                    
 //                    self.travelTime = route.expectedTravelTime
                     
                     self.adivsoryNotices = route.advisoryNotices
@@ -173,7 +195,8 @@ struct MapView: UIViewRepresentable {
                     
                     let dist = route.distance
                     print(String(format: "%.02f", convertKMtoMiles(from: dist)), "miles")
-
+                    
+                    
 //                directions.calculateETA { (etaResponse, err) in
 //                    if let error = err {
 //                        print(error.localizedDescription)
@@ -182,7 +205,7 @@ struct MapView: UIViewRepresentable {
 //                    }
                 }
             }
-        }
+//        }
     }
     private func convertKMtoMiles(from meter: Double) -> Double {
         let conversionRate = (meter / 1000) * 0.621371
@@ -192,21 +215,73 @@ struct MapView: UIViewRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
-
-        init(_ parent: MapView) {
-            self.parent = parent
+    
+}
+class Coordinator: NSObject, MKMapViewDelegate, ObservableObject {
+    @Published var addressLabel: String?
+    var locationManager = LocationManager()
+    var previousLoc: CLLocation?
+    
+    var parent: MapView?
+    
+    
+    init(_ parent: MapView) {
+        
+        self.parent = parent
+    }
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        //            passed from @Binding in MapView(which is passed from main view)
+        parent!.centerCoordinate = mapView.centerCoordinate
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor(#colorLiteral(red: 0, green: 0.6509803922, blue: 1, alpha: 1))
+        renderer.lineWidth = 5
+        return renderer
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    
+    
+    //        Geocoding
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        locationManager.$initialLocation.sink { (loc) in
+            self.previousLoc = loc!
+            print("previous location: \(self.previousLoc)")
         }
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            parent.centerCoordinate = mapView.centerCoordinate
-        }
-        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-            let renderer = MKPolylineRenderer(overlay: overlay)
-            renderer.strokeColor = UIColor(#colorLiteral(red: 0, green: 0.6509803922, blue: 1, alpha: 1))
-            renderer.lineWidth = 5
-            return renderer
+        
+        guard center.distance(from: previousLoc!) > 50 else { return }
+        
+        previousLoc = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let err = error {
+                print("false")
+            }
+            guard let placemark = placemarks?.first else {
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel = String("\(streetNumber) @ \(streetName)")
+                print(self.addressLabel as Any)
+            }
         }
     }
+    
 }
+
